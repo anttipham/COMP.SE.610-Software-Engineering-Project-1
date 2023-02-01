3a8b3404cacd8e587a261d544e158ff5ed8efd1c
Antti Pham <antti.pham@tuni.fi>
2023-02-01 20:52:04 +0200

    Edit main.py to use the timer

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
-print("Hello World!")
+"""
+Univincity-throw-in-bot
+"""
+import timer
+
+
+def main():
+    """
+    Main function
+    """
+    timer.run_with_interval(lambda: print('Hello World!'), 1)
+
+
+if __name__ == '__main__':
+    main()
