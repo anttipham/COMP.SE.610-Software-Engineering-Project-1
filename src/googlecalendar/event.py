381b8b5349c24072d4d0a719bd735b25172cae16
Antti Pham <antti.pham@tuni.fi>
2023-02-01 19:16:10 +0200

    Make empty functions in Google Calendar

---
+++ src/googlecalendar/event.py
@@ -0,0 +1,11 @@
+"""
+Handle Google Calendar events
+"""
+
+# def get_participants():
+#     """Returns calendar event participants"""
+#     pass
+# def add_participant():
+#     """Adds a participant to the calendar event"""
+# def remove_participant():
+#     """Removes a participant from the calendar event"""
