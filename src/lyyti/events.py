6d714a7b06fd3cd7639a9dab6db4d8d3356cd857
Antti Pham <antti.pham@tuni.fi>
2023-02-14 12:01:53 +0200

    Parse Google Group link in Lyyti API events

--- src/lyyti/events.py
+++ src/lyyti/events.py
@@ -3,6 +3,7 @@ Handles the events of the Lyyti API.
 """
 
 from dataclasses import dataclass
+from typing import Any
 from .participants import Participant, load_participants
 from .lyytirequest import get_events
 
@@ -11,10 +12,29 @@ from .lyytirequest import get_events
 class Event:
     """Contains information of event"""
     event_id: str
-    google_group_link: str
     participants: list[Participant]
+    google_group_link: str
     # slack_members: list[str]  # For updating participants in Slack
 
+
+def find_google_group_link(custom: dict[str, dict[str, Any]]) -> str:
+    """
+    From the given custom field in JSON response, return the Google Group link.
+    If there's no Google Group link, returns an empty string.
+
+    Args:
+        custom (dict[str, dict[str, Any]]): Custom field
+
+    Returns:
+        str: Google Group link if found, otherwise empty string.
+    """
+    custom_contents = custom['custom'].values()
+    for content in custom_contents:
+        if content['title'] == 'Google Group link':
+            google_group_link = content['answer']
+            return google_group_link
+    # Couldn't find Google Group link
+    return ''
 
 
 def load_events() -> list[Event]:
@@ -27,8 +47,8 @@ def load_events() -> list[Event]:
     json_object = get_events()
 
     return [Event(event_id=data['eid'],
-                  google_group_link=data['custom']['3448']['answer'],
-                  participants=load_participants(data['eid']))
+                  participants=load_participants(data['eid']),
+                  google_group_link=find_google_group_link(data['custom']))
             for data in json_object['results'].values()]
 
 
