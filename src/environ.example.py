5475977e872c45205acef4a23a5d71f00edfacf4
Antti Pham <antti.pham@tuni.fi>
2023-02-16 12:07:24 +0200

    Update environ.example.py text

--- src/environ.example.py
+++ src/environ.example.py
@@ -4,10 +4,10 @@ Sets variables to environment variables for testing purposes.
 import os
 
 if 'LYYTI_PRIVATE_KEY' not in os.environ:
-    os.environ['LYYTI_PRIVATE_KEY'] = '<very private>'
+    os.environ['LYYTI_PRIVATE_KEY'] = '<PRIVATE KEY HERE>'
 
 if 'LYYTI_PUBLIC_KEY' not in os.environ:
-    os.environ['LYYTI_PUBLIC_KEY'] = '<very public>'
+    os.environ['LYYTI_PUBLIC_KEY'] = '<PUBLIC KEY HERE>'
 
 if 'TIMEOUT_DURATION' not in os.environ:
     os.environ['TIMEOUT_DURATION'] = '10'
