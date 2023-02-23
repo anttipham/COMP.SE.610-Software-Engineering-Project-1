bc628abb2a718efbc3192dac3d1ad1fb4a0f50cf
Milo Brown <milo.brown@tuni.fi>
2023-02-23 17:10:15 +0200

    modify return value of lyytirequests

--- src/lyyti/lyytirequest.py
+++ src/lyyti/lyytirequest.py
@@ -12,7 +12,7 @@ import requests
 
 import environ  # Set environment variables
 
-API_ROOT = os.environ['LYYTI_ROOT_URL']
+API_ROOT = os.environ["LYYTI_ROOT_URL"]
 
 
 def generate_headers(call_string: str) -> dict[str, str]:
@@ -31,55 +31,58 @@ def generate_headers(call_string: str) -> dict[str, str]:
     Returns:
         dict[str, str]: Returns the authorization header.
     """
-    public_key = os.environ['LYYTI_PUBLIC_KEY']
-    private_key = os.environ['LYYTI_PRIVATE_KEY']
+    public_key = os.environ["LYYTI_PUBLIC_KEY"]
+    private_key = os.environ["LYYTI_PRIVATE_KEY"]
     timestamp = str(int(time.time()))
-    msg = ','.join([public_key, timestamp, call_string])
+    msg = ",".join([public_key, timestamp, call_string])
 
     signature = hmac.new(
-        private_key.encode('utf-8'),
-        msg=base64.b64encode(msg.encode('utf-8')),
-        digestmod=hashlib.sha256
+        private_key.encode("utf-8"),
+        msg=base64.b64encode(msg.encode("utf-8")),
+        digestmod=hashlib.sha256,
     ).hexdigest()
 
     headers = {
-        'Accept': 'application/json; charset=utf-8',
-        'Authorization': f'LYYTI-API-V2 public_key={public_key}, ' +
-                         f'timestamp={timestamp}, ' +
-                         f'signature={signature}',
+        "Accept": "application/json; charset=utf-8",
+        "Authorization": f"LYYTI-API-V2 public_key={public_key}, "
+        + f"timestamp={timestamp}, "
+        + f"signature={signature}",
     }
 
     return headers
 
 
-def get_events() -> dict:
+def get_events() -> requests.Response:
     """
-    Gets events from Lyyti API using requests package and returns the json data
-    in dictionaries and lists using json package.
+    Gets events from Lyyti API using requests package and returns
+    the response as requests.Response object
 
     Returns:
-        dict: JSON data in dictionaries and lists.
+        requests.Response object: contains the response to HTTP request.
     """
-    call_string = os.environ['LYYTI_EVENTS_CALLSTRING']
+    call_string = os.environ["LYYTI_EVENTS_CALLSTRING"]
     url = API_ROOT + call_string
-    response = requests.get(url,
-                            headers=generate_headers(call_string),
-                            timeout=float(os.environ['TIMEOUT_DURATION']))
-    return response.json()
+    response = requests.get(
+        url,
+        headers=generate_headers(call_string),
+        timeout=float(os.environ["TIMEOUT_DURATION"]),
+    )
+    return response
 
 
-def get_participants(event_id: str) -> dict:
+def get_participants(event_id: str) -> requests.Response:
     """
     Gets participants from an event by its event ID from Lyyti API using
-    requests package and returns the json data in dictionaries and lists using
-    json package.
+    requests package and returns the response as requests.Response object.
 
     Returns:
-        dict: JSON data in dictionaries and lists.
+        requests.Response object: contains the response to HTTP request.
     """
-    call_string = os.environ['LYYTI_PARTICIPANTS_CALLSTRING'].format(event_id)
+    call_string = os.environ["LYYTI_PARTICIPANTS_CALLSTRING"].format(event_id)
     url = API_ROOT + call_string
-    response = requests.get(url,
-                            headers=generate_headers(call_string),
-                            timeout=float(os.environ['TIMEOUT_DURATION']))
-    return response.json()
+    response = requests.get(
+        url,
+        headers=generate_headers(call_string),
+        timeout=float(os.environ["TIMEOUT_DURATION"]),
+    )
+    return response
