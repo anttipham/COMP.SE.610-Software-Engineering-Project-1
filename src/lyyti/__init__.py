a4eba2c03f5a51f4f8ac9024f76f48ea437fc139
Milo Brown <milo.brown@tuni.fi>
2023-02-23 17:34:47 +0200

    revert changes to __init__.py

--- src/lyyti/__init__.py
+++ src/lyyti/__init__.py
@@ -5,5 +5,4 @@ The main use case is to call load_events which does all the work for
 getting events and the participants.
 """
 from .events import Event, load_events
-from .lyytirequest import get_events, get_participants
 from .participants import Participant
