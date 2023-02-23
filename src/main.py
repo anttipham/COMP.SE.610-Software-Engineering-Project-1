fe3aefab99a6660188d93fc74b1b8bbabaced5a2
Milo Brown <milo.brown@tuni.fi>
2023-02-23 17:11:12 +0200

    add print for participants in main

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ Univincity-throw-in-bot
 """
 import dataclasses
 import json
+
 import lyyti
 
 
@@ -12,11 +13,18 @@ def main():
     """
     events = lyyti.load_events()
     # Printing the events in a nice format.
+    print("Events:\n")
     print(json.dumps([dataclasses.asdict(e) for e in events], indent=4))
+
+    # Printing the participants of each event in nice format.
+    print("Event participants:\n")
+
     for event in events:
-        # Handle events
-        pass
+        print(f"Event: {event.event_id}\nParticipants:", end=" ")
+        for participant in event.participants:
+            print(participant.email, end=" || ")
+        print()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
