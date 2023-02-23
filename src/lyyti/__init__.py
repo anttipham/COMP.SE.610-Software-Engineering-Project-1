60a98e6371a542a5b7d19c0264ad2df44201e3ab
Milo Brown <milo.brown@tuni.fi>
2023-02-23 17:08:22 +0200

    add imports for testing

--- src/lyyti/__init__.py
+++ src/lyyti/__init__.py
@@ -4,5 +4,6 @@ This package handles Lyyti API requests.
 The main use case is to call load_events which does all the work for
 getting events and the participants.
 """
-from .events import load_events, Event
+from .events import Event, load_events
+from .lyytirequest import get_events, get_participants
 from .participants import Participant
