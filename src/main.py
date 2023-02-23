6e72b2d9bece903cf3b92aded8cc3dd8b771e130
Antti Pham <antti.pham@tuni.fi>
2023-02-23 12:54:11 +0200

    print internal data as JSON in main for now

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 """
 Univincity-throw-in-bot
 """
+import dataclasses
+import json
 import lyyti
 
 
@@ -9,6 +11,8 @@ def main():
     Main function
     """
     events = lyyti.load_events()
+    # Printing the events in a nice format.
+    print(json.dumps([dataclasses.asdict(e) for e in events], indent=4))
     for event in events:
         # Handle events
         pass
