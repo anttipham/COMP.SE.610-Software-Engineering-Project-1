95768a7310b850822b5dc65999b12f59db09013c
Antti Pham <antti.pham@tuni.fi>
2023-03-09 17:08:30 +0200

    Add function to update groups members

--- src/googleservices/groups.py
+++ src/googleservices/groups.py
@@ -2,6 +2,26 @@
 Handles calendar events in Google Groups
 """
 
-def get_calendar_events():
+import google.auth
+from googleapiclient.discovery import build
+
+
+def update_groups(group_id: str, emails: list[str]):
     """Gets the calendar events from Google Groups"""
-    pass
+    # Authenticate the API request using your Google Cloud Console credentials
+    creds, project_id = google.auth.default(
+        scopes=["https://www.googleapis.com/auth/apps.groups.settings"]
+    )
+
+    # Create a service object to interact with the Google Groups API
+    with build("groupssettings", "v1", credentials=creds) as service:
+        # Use the members.insert() method to add the member to the group
+        for email in emails:
+            response = (
+                service.members()
+                .insert(groupUniqueId=group_id,
+                        body={"email": email, "role": "MEMBER"})
+                .execute()
+            )
+            # Debug: Confirm that the member was added successfully
+            print(response)
