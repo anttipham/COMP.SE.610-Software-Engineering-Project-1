d33fd6fae592fd6f7bfbb7ebb297ae8c32daff50
Antti Pham <antti.pham@tuni.fi>
2023-03-10 00:14:54 +0200

    Dont catch all errors and return None. Just let errors interrupt program

--- src/googleservices/buildservice.py
+++ src/googleservices/buildservice.py
@@ -10,19 +10,14 @@ def build_google_service(api_name: str, api_version: str) -> Resource:
     Returns:
             object: The google api service build.
     """
-    try:
-        # Get credentials.
-        cred = google.auth.default(
-            scopes=[
-                "https://www.googleapis.com/auth/calendar",
-                "https://www.googleapis.com/auth/calendar.events",
-                "https://www.googleapis.com/auth/apps.groups.settings",
-            ]
-        )[0]
+    # Get credentials.
+    cred = google.auth.default(
+        scopes=[
+            "https://www.googleapis.com/auth/calendar",
+            "https://www.googleapis.com/auth/calendar.events",
+            "https://www.googleapis.com/auth/apps.groups.settings",
+        ]
+    )[0]
 
-        # Get session.
-        return build(api_Name, api_version, credentials=cred)
-
-    # Handle other errors.
-    except:
-        print("Not implemented! Handles all other errors.")
+    # Get session.
+    return build(api_name, api_version, credentials=cred)
