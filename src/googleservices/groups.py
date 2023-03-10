046f073b6d968c8b5c2d5eddf751cdac8ff644ec
Antti Pham <antti.pham@tuni.fi>
2023-03-10 18:17:50 +0200

    Add function body for removing emails from Google Groups

--- src/googleservices/groups.py
+++ src/googleservices/groups.py
@@ -22,3 +22,15 @@ def add_emails_to_group(group_id: str, emails: list[str]) -> None:
             .execute()
         )
         print(response)
+
+
+def remove_emails_from_group(group_id: str, emails: list[str]) -> None:
+    """
+    This function takes a Google Group ID and a list of emails and removes the emails from
+    the group.
+
+    Args:
+        group_id (str): The Google Group ID of the group to remove members from
+        emails (list): List of emails to remove from the group
+    """
+    pass
