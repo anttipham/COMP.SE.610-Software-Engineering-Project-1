bce22d427b97eead069e14c8e35e8820e44dc506
Antti Pham <antti.pham@tuni.fi>
2023-03-15 21:10:48 +0200

    Make service account json path into absolute path

--- src/environ.example.py
+++ src/environ.example.py
@@ -2,21 +2,24 @@
 Sets variables to environment variables for testing purposes.
 """
 import os
+import pathlib
 
 def set_environ() -> None:
     """
     Sets the environment variables for testing purposes.
     """
-    if "GOOGLE_APPLICATION_CREDENTIALS" not in os.environ:
-        os.environ[
-            "GOOGLE_APPLICATION_CREDENTIALS"
-        ] = "univincity-throw-in-bot-018d57429b27.json"
-
     if "LYYTI_PRIVATE_KEY" not in os.environ:
         os.environ["LYYTI_PRIVATE_KEY"] = "<PRIVATE KEY HERE>"
 
     if "LYYTI_PUBLIC_KEY" not in os.environ:
         os.environ["LYYTI_PUBLIC_KEY"] = "<PUBLIC KEY HERE>"
+
+    if "GOOGLE_APPLICATION_CREDENTIALS" not in os.environ:
+        project_root = pathlib.Path(__file__).parent.parent
+        service_account_credentials = project_root / "univincity-throw-in-bot-018d57429b27.json"
+        os.environ[
+            "GOOGLE_APPLICATION_CREDENTIALS"
+        ] = str(service_account_credentials)
 
     if "TIMEOUT_DURATION" not in os.environ:
         os.environ["TIMEOUT_DURATION"] = "10"
