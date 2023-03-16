c0fd4b1c40059d7cda9ad2ac61363ab826a0a477
Antti Pham <antti.pham@tuni.fi>
2023-03-16 23:42:55 +0200

    Add function for getting calendar events

--- src/googleservices/calendarevents.py
+++ src/googleservices/calendarevents.py
@@ -1,7 +1,46 @@
 """
 Contains functions that affect Google Calendar
 """
+from dataclasses import dataclass
+
 from .buildservice import build_google_service
+
+
+@dataclass
+class CalendarEvent:
+    """Represents a calendar event."""
+
+    name: str
+    id: str
+
+
+def get_calendar_events(calendar_id: str) -> list[CalendarEvent]:
+    """
+    This function takes a Google Calendar ID and returns a list of all events in the
+    calendar.
+
+    Args:
+        calendar_id (str): The ID of the Google Calendar to fetch events from
+
+    Raises:
+        googleapiclient.errors.HttpError: If the function fails.
+    """
+    service = build_google_service("calendar", "v3")
+
+    events_result = (
+        service.events()
+        .list(
+            calendarId=calendar_id,
+            singleEvents=False,
+            orderBy="startTime",
+        )
+        .execute()
+    )
+    event_items = events_result.get("items", [])
+    events = [
+        CalendarEvent(id=item["id"], name=item["summary"]) for item in event_items
+    ]
+    return events
 
 
 def update_calendar_event_participants(
