0d9433bcd51d40834c98cee39184a8809b998001
Antti Pham <antti.pham@tuni.fi>
2023-03-16 23:46:00 +0200

    Change update_calendar_event_participants to use patch instead of update. Change its parameter to "calendar_id: str, event_id: str, participants: list[str]". Change docstring

--- src/googleservices/calendarevents.py
+++ src/googleservices/calendarevents.py
@@ -44,25 +44,27 @@ def get_calendar_events(calendar_id: str) -> list[CalendarEvent]:
 
 
 def update_calendar_event_participants(
-    calendar_id: str, event_id: str, request_body: dict
+    calendar_id: str, event_id: str, participants: list[str]
 ) -> None:
     """
-    This function updates the participants of a Google Calendar event
+    This function updates the participants of a Google Calendar event.
+
+    Participants will receive notifications that they have been invited to the event.
 
     Args:
         calendar_id (str): The calendar ID of the calendar to update
         event_id (str): The ID of the event to update
-        request_body (dict): TODO: _description_
+        participants (list[str]): List of participant email addresses
+
+    Raises:
+        googleapiclient.errors.HttpError: If the function fails.
     """
+    participants_body = [{"email": email} for email in participants]
 
     service = build_google_service("calendar", "v3")
-    response = (
-        service.events()
-        .update(
-            calendarId=calendar_id,
-            eventId=event_id,
-            body=request_body,
-        )
-        .execute()
-    )
-    print(response)
+    service.events().patch(
+        calendarId=calendar_id,
+        eventId=event_id,
+        body={"attendees": [participants_body]},
+        sendUpdates="all",
+    ).execute()
