16a50744e03a27c066a37108c95f50cbe7c1d552
Milo Brown <milo.brown@tuni.fi>
2023-04-25 15:24:18 +0300

    add unit tests for tryexceptlog

--- test/utils_test.py
+++ test/utils_test.py
@@ -1,9 +1,10 @@
 """
 Tests for the functions in utils module.
 """
+
 import pytest
 
-from utils import extract_group_id
+from utils import extract_group_id, tryexceptlog
 
 
 class TestExtractGroupId:
@@ -35,3 +36,45 @@ class TestExtractGroupId:
         with pytest.raises(ValueError):
             extract_group_id(url)
 
+
+import traceback
+from unittest import mock
+
+
+class TestTryExceptLog:
+    """Tests for the tryexceptlog context manager."""
+
+    def test_tryexceptlog_no_exception(self):
+        """Test that tryexceptlog does not log anything when no exception is raised."""
+
+        # Create a variable to store the log output
+        log_output = ""
+
+        # Mock botlog() method
+        botlog_mock = mock.MagicMock()
+        with mock.patch("utils.botlog", botlog_mock):
+            # Test tryexceptlog with no exception
+            with tryexceptlog():
+                log_output = "No exception was raised"
+
+            # Verify that no exception was raised and no log message was generated
+            botlog_mock.assert_not_called()
+            assert log_output == "No exception was raised"
+
+    def test_tryexceptlog_with_exception(self):
+        """Test that tryexceptlog logs the exception when an exception is raised."""
+
+        # Create a variable to store the log output
+        log_output = ""
+
+        # Mock botlog() method
+        botlog_mock = mock.MagicMock()
+        with mock.patch("utils.botlog", botlog_mock):
+            # Test tryexceptlog with an exception
+            with tryexceptlog():
+                try:
+                    raise ValueError("Test exception")
+                except ValueError:
+                    log_output = traceback.format_exc()
+            assert "ValueError: Test exception" in log_output
+            assert "Traceback (most recent call last):" in log_output
