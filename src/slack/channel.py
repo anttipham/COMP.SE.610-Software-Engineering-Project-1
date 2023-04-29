d5970be3def4da3a07561df5b18517e82922c070
Samundra Dhakal <samundra.dhakal@tuni.fi>
2023-04-29 13:37:40 +0300

    change list to set as it make code checking faster

--- src/slack/channel.py
+++ src/slack/channel.py
@@ -1,10 +1,5 @@
-"""
-Handles the authentication of Slack API and the use of Slack features to add and remove the participants in the channel.
-"""
-
 import slack
 import os
-import pip._vendor.requests
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -18,42 +13,32 @@ we need to have following user token scope in the slackBot:
 5. users:read.email
 6. admin.conversations:write
 """
-
 # Get credentials.
 env_path = Path(".") / ".env"
 load_dotenv(dotenv_path=env_path)
 client = slack.WebClient(token=os.environ["USER_TOKEN"])
-"""
-you can remove 3 rows above if you want to insert your slack token directly
-to the code and use the following row instead. Replace "your token" with your slack bot token
-client = slack.WebClient(token="your token") 
-"""
 
-
-def get_all_user_IDs(email) -> list[str]:
+def get_all_user_IDs(emails: list[str]) -> set[str]:
     """
-    Following method gets all the user Ids from the Workspace
-    that matches the email address from lyyti event participant list parameter emails is a list of email address
-    corresponding the member list in lyyti event Method return a list of IDs
-
+    Get all the user Ids from the Workspace
+    that match the email addresses in the input list.
+    Return a set of IDs.
     Args:
         email (str): The email to use
 
     Returns:
         list[str]:  Gets all the user Ids from the Workspace
     """
-    id_list = []
-    for email in list:
+    id_set = set()
+    for email in emails:
         response = client.users_lookupByEmail(email=email)
-        id_list.append(response["user"]["id"])
+        id_set.add(response["user"]["id"])
+    return id_set
 
-    return id_list
-
-
-def get_user_ids_from_channel(channel_id: str) -> list[str]:
+def get_user_ids_from_channel(channel_id: str) -> set[str]:
     """
-    This method gets all the users IDs from specific channel.
-
+    Get all the user IDs from a specific channel.
+    Return a set of user IDs.
     Args:
         channelId (str): The Channel Id.
 
@@ -62,13 +47,11 @@ def get_user_ids_from_channel(channel_id: str) -> list[str]:
     """
     response = client.conversations_members(channel=channel_id)
     member_ids = response["members"]
-    return member_ids
+    return set(member_ids)
 
-
-def invite_members(emailList: list[str], channelId: str):
+def invite_members(emails: list[str], channel_id: str):
     """
-    This method invite all the users with their email address to specific channel.
-
+    Invite all the users with their email addresses to a specific channel.
     Args:
         emailList list[str]: The list of the email address.
         channelId (str): The Channel Id.
@@ -76,23 +59,19 @@ def invite_members(emailList: list[str], channelId: str):
     Returns:
         list[str]:  Return lists of user Ids that are already invited to the channel
     """
-    all_id_list = get_all_user_IDs(emailList)
-    members_in_channel = get_user_ids_from_channel(channelId)
+    all_id_set = get_all_user_IDs(emails)
+    members_in_channel_set = get_user_ids_from_channel(channel_id)
 
-    for i in members_in_channel[:]:
-        if i in all_id_list:
-            members_in_channel.remove(i)
-            all_id_list.remove(i)
+    new_members_set = all_id_set - members_in_channel_set
+    kicked_members_set = members_in_channel_set - all_id_set
 
-    for member in all_id_list:
-        client.admin_conversations_invite(channel=channelId, user=member)
+    for member in new_members_set:
+        client.admin_conversations_invite(channel=channel_id, user=member)
 
-    if members_in_channel:
-        for member in members_in_channel:
-            client.conversations_kick(channel=channelId, user=member)
-
-    return
-
+    for member in kicked_members_set:
+        client.conversations_kick(channel=channel_id, user=member)
 
 if __name__ == "__main__":
-    invite_members()
+    emails = ["email1@example.com", "email2@example.com"]
+    channel_id = "ABC123"
+    invite_members(emails, channel_id)
